2ac7f26bd531d8fef0ce2931480804b3cb1ac844
Daymian Tomczyk <daymian.tomczyk@outlook.com>
2022-01-30 23:22:07 -0500

    Operating system boots

---
+++ src/impl/x86_64/boot/header.asm
@@ -0,0 +1,16 @@
+section .multiboot_header
+header_start:
+	; magicnumber
+	dd 0xe85250d6
+	; architecture
+	dd 0 ;protected mode i386
+	; header length
+	dd header_end - header_start
+	; checksum
+	dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
+
+	;end tag
+	dw 0
+	dw 0
+	dw 8
+header_end:
